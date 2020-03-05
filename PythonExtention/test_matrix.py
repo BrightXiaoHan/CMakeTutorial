dce5e613c249fbd214ad05911997d563ea096926
hanbing <beatmight@gmail.com>
2020-03-05 17:34:46 +0800

    增加了一些测试用例

--- PythonExtention/test_matrix.py
+++ PythonExtention/test_matrix.py
@@ -1,18 +1,57 @@
+import unittest
 import matrix
-
-shape = 10
-
-mat_a = matrix.random(shape, shape)
-mat_b = matrix.random(shape, shape)
-
-print(mat_a * mat_b)
-print(mat_a + mat_b)
-print(mat_a - mat_b)
-
-print(mat_a.to_list())
-print(mat_a.data)
-print(mat_a.row)
-print(mat_a.colunm)
+import random
 
 
+class TestMatrix(unittest.TestCase):
 
+    def test_initialize(self):
+        mat_a = matrix.random(2, 3)
+        mat_b = matrix.ones(2, 3)
+        mat_c = matrix.zeros(2, 3)
+
+        assert mat_a.colunm == mat_b.colunm == mat_c.colunm == 3
+        assert mat_a.row == mat_b.row == mat_c.row == 2
+
+    def test_matmul(self):
+        m = random.randint(2, 100)
+        k = random.randint(2, 100)
+        n = random.randint(2, 100)
+
+        mat_a = matrix.random(m, k)
+        mat_b = matrix.random(k, n)
+        mat_c = mat_a * mat_b
+        
+        self.assertEqual(
+            [mat_c.colunm, mat_c.row],
+            [n, m]
+        )
+
+    def test_add(self):
+        m = random.randint(2, 100)
+        n = random.randint(2, 100)
+        
+        mat_a = matrix.random(m, n)
+        mat_b = matrix.random(m, n)
+        mat_c = mat_a + mat_b
+        
+        self.assertEqual(
+            [mat_c.colunm, mat_c.row],
+            [n, m]
+        )
+        
+    def test_minus(self):
+        m = random.randint(2, 100)
+        n = random.randint(2, 100)
+        
+        mat_a = matrix.random(m, n)
+        mat_b = matrix.random(m, n)
+        mat_c = mat_a - mat_b
+        
+        self.assertEqual(
+            [mat_c.colunm, mat_c.row],
+            [n, m]
+        )
+    
+if __name__ == "__main__":
+    unittest.main()
