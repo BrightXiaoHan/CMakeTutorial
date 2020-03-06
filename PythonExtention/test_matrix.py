4926d8dd6bed8428e04064157b4a474c4d557df2
hanbing <beatmight@gmail.com>
2020-03-06 14:19:50 +0800

    增加了attr test

--- PythonExtention/test_matrix.py
+++ PythonExtention/test_matrix.py
@@ -2,6 +2,8 @@ import unittest
 import matrix
 import random
 
+import numpy as np
+
 
 class TestMatrix(unittest.TestCase):
 
@@ -21,7 +23,7 @@ class TestMatrix(unittest.TestCase):
         mat_a = matrix.random(m, k)
         mat_b = matrix.random(k, n)
         mat_c = mat_a * mat_b
-        
+
         self.assertEqual(
             [mat_c.colunm, mat_c.row],
             [n, m]
@@ -30,28 +32,37 @@ class TestMatrix(unittest.TestCase):
     def test_add(self):
         m = random.randint(2, 100)
         n = random.randint(2, 100)
-        
+
         mat_a = matrix.random(m, n)
         mat_b = matrix.random(m, n)
         mat_c = mat_a + mat_b
-        
+
         self.assertEqual(
             [mat_c.colunm, mat_c.row],
             [n, m]
         )
-        
+
     def test_minus(self):
         m = random.randint(2, 100)
         n = random.randint(2, 100)
-        
+
         mat_a = matrix.random(m, n)
         mat_b = matrix.random(m, n)
         mat_c = mat_a - mat_b
-        
+
         self.assertEqual(
             [mat_c.colunm, mat_c.row],
             [n, m]
         )
-    
+
+    def test_attr(self):
+        m = np.ones((10, 10))
+        n = matrix.ones(10, 10)
+
+        assert n.colunm == 10
+        assert n.row == 10
+        self.assertEqual(n.data, m.tolist())
+
+
 if __name__ == "__main__":
     unittest.main()
